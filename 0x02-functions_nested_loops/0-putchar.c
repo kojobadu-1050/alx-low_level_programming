e4c2e709aea2d53e3ea8aeeeb5fe58d0b8a6e57b
kojobadu-1050 <paulbadu1050@gmail.com>
2023-02-22 03:00:28 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar
- *
+ * main - prints _putchar and a new line.
  * Return: Always 0.
  */
 int main(void)
@@ -18,4 +17,3 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-}
